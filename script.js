629355af43bd1402b57d14e9d75d95a3e05bac75
Melyssa <melcastro417@gmail.com>
2025-04-02 15:51:10 -0300

    mensagem

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-let teste= 'ola'
-console.log(teste)
+// Criando variáveis
+let nome = "Melyssa";
+let idade = 19;
+let cidade = "São Luís";
+
+// Exibindo no console
+console.log(nome);   // Exibe: Melyssa
+console.log(idade);  // Exibe: 19
+console.log(cidade); // Exibe: São Luís
+
+// Criando variáveis
+nome = "Melyssa";
+let sobrenome = "Castro" 
+
+console.log(nome+ " " + sobrenome); // Exibe: Melyssa 
+
+nome = "Melyssa"
+let hobby = "pintar"
+
+console.log("Eu sou " + nome + " e meu hobby é " + hobby + ".");
